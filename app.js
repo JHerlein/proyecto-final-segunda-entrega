505cc31267142cf8cd702f6d40755791dc2b23a8
Jherlein <jherlein12@gmail.com>
2022-05-13 10:49:23 -0400

    deleted comments

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const ProductosMongoDB = require('./daos/productos/productosMongoDB')
 const CartsFirebase = require('./daos/carts/cartsFirebase')
 const ProductosFirebase = require('./daos/productos/productosFirebase')
 
-// const port = 8080
-
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use('/api/productos',routerProduct)
@@ -20,59 +18,8 @@ app.use(express.static('./public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 
-
 app.listen(process.env.PORT,console.log(`Listening on port ${process.env.PORT}`))
-
 
 app.get('/products',(req,res) => {    
     res.render('products.html')
 })
-
-// connectionFirebaseProductos = new ProductosFirebase()
-// connectionFirebaseMensajes = new MensajesFirebase()
-
-
-// connectionMongoCarts = new CartsMongoDB()
-// connectionMongoProductos = new ProductosMongoDB()
-// connectionFirebaseCarts = new CartsFirebase()
-// connectionFirebaseProductos = new ProductosFirebase()
-// connectionMongoCarts.listarAll()
-// connectionMongoProductos.listarAll()
-// newData = {    
-//     timestamp: new Date(),
-//     productos: [
-//       {
-//         id: 2,
-//         timestamp: 123,
-//         nombre: "Cuchillo",
-//         descripcion: "Cuchillo",
-//         codigo: "CU",
-//         foto: "foto",
-//         precio: 150,
-//         stock: 2
-//       },
-//       {
-//         id: 1,
-//         timestamp: 123,
-//         nombre: "Plato",
-//         descripcion: "Un plato",
-//         codigo: "PL",
-//         foto: "foto",
-//         precio: 150,
-//         stock: 2
-//       }
-//     ]
-//   }
-// connectionFirebaseCarts.create(newData)
-
-// connectionFirebaseMensajes.listarAll().then(result => {
-//     console.log(result)
-// })
-
-// connectionFirebaseProductos.listarAll().then(result => {
-//     console.log(result)
-// })
-
-// const db = PROCESS.ENV.TIPO_DB || 'mongodb'
-
-
